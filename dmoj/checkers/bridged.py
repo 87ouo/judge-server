87ouo/judge-server

commit 0c0875730356efb388a39c788c2a30909d2b0691

checker: Fix errors in `bridged` checker (#595)

dmoj/checkers/bridged.py
@@ -7,45 +7,28 @@ from dmoj.result import CheckerResult
 from dmoj.utils.aux_files import compile_with_auxiliary_files, mktemp
 from dmoj.utils.unicode import utf8text
 
-checker_defaults = {
-    'time_limit': env['generator_time_limit'],
-    'memory_limit': env['generator_memory_limit'],
-    'compiler_time_limit': env['generator_compiler_limit'],
-    'feedback': True,
-}
-
 executor = None
 
 
-def get_executor(checker_kwargs, problem_id):
+def get_executor(files, lang, compiler_time_limit, problem_id):
     global executor
 
     if executor is None:
-        if 'files' not in checker_kwargs:
-            raise InternalError('no checker file(s) specified!')
-        if 'lang' not in checker_kwargs:
-            raise InternalError('language not specified for checker!')
-
-        filenames = list(map(lambda x: os.path.join(get_problem_root(problem_id), x), checker_kwargs['files']))
-        lang = checker_kwargs['lang']
-        executor = compile_with_auxiliary_files(filenames, lang, checker_kwargs['compiler_time_limit'])
+        filenames = [os.path.join(get_problem_root(problem_id), f) for f in files]
+        executor = compile_with_auxiliary_files(filenames, lang, compiler_time_limit)
 
     return executor
 
 
-def check(process_output, judge_output, judge_input, checker_kwargs,
-          problem_id, point_value=None, **kwargs) -> CheckerResult:
-
-    # Update checker_kwargs with defaults
-    for key, value in checker_defaults.items():
-        checker_kwargs.setdefault(key, value)
-
-    executor = get_executor(checker_kwargs, problem_id)
+def check(process_output, judge_output, judge_input, problem_id,
+          files, lang, time_limit=env['generator_time_limit'], memory_limit=env['generator_memory_limit'],
+          compiler_time_limit=env['generator_compiler_limit'], feedback=True,
+          point_value=None, **kwargs) -> CheckerResult:
+    executor = get_executor(files, lang, compiler_time_limit, problem_id)
 
     with mktemp(judge_input) as input_file, mktemp(process_output) as output_file, mktemp(judge_output) as judge_file:
         process = executor.launch(input_file.name, output_file.name, judge_file.name, stdout=subprocess.PIPE,
-                                  stderr=subprocess.PIPE, memory=checker_kwargs['memory_limit'],
-                                  time=checker_kwargs['time_limit'])
+                                  stderr=subprocess.PIPE, memory=memory_limit, time=time_limit)
 
         proc_output, error = map(utf8text, process.communicate())
 
@@ -56,12 +39,12 @@ def check(process_output, judge_output, judge_input, checker_kwargs,
         IE = 3
 
         if process.returncode == AC:
-            if checker_kwargs['feedback']:
+            if feedback:
                 return CheckerResult(True, point_value, feedback=proc_output)
             else:
                 return CheckerResult(True, point_value)
         elif process.returncode in (WA, PE):
-            if checker_kwargs['feedback']:
+            if feedback:
                 return CheckerResult(False, 0, feedback=proc_output)
             else:
                 return CheckerResult(False, 0, feedback='Presentation Error' if process.returncode == PE else '')

commit 57b8cd0d37f5f3c975b0a5c54f8fcc41f257f7c0

Fix `compile_with_auxiliary_files`
* Allows for passing of flags to generators and bridged checkers
* Allows for generators and bridged checkers to be not cached
* Fixes regression with bridged checker introduced in 0c08757
* Convert `dmoj.utils.aux_files` to Unix line endings

dmoj/checkers/bridged.py
@@ -10,23 +10,26 @@ from dmoj.utils.unicode import utf8text
 executor = None
 
 
-def get_executor(files, lang, compiler_time_limit, problem_id):
+def get_executor(files, lang, flags, cached, compiler_time_limit, problem_id):
     global executor
 
     if executor is None:
-        if not isinstance(files, list):
-            files = [files]
-        filenames = [os.path.join(get_problem_root(problem_id), f) for f in files]
-        executor = compile_with_auxiliary_files(filenames, lang, compiler_time_limit)
+        if isinstance(files, str):
+            filenames = [files]
+        elif isinstance(files.unwrap(), list):
+            filenames = list(files.unwrap())
+
+        filenames = [os.path.join(get_problem_root(problem_id), f) for f in filenames]
+        executor = compile_with_auxiliary_files(filenames, flags, lang, compiler_time_limit, cached)
 
     return executor
 
 
 def check(process_output, judge_output, judge_input, problem_id,
           files, lang, time_limit=env['generator_time_limit'], memory_limit=env['generator_memory_limit'],
-          compiler_time_limit=env['generator_compiler_limit'], feedback=True,
+          compiler_time_limit=env['generator_compiler_limit'], flags=[], feedback=True, cached=True,
           point_value=None, **kwargs) -> CheckerResult:
-    executor = get_executor(files, lang, compiler_time_limit, problem_id)
+    executor = get_executor(files, lang, flags, cached, compiler_time_limit, problem_id)
 
     with mktemp(judge_input) as input_file, mktemp(process_output) as output_file, mktemp(judge_output) as judge_file:
         process = executor.launch(input_file.name, output_file.name, judge_file.name, stdout=subprocess.PIPE,

dmoj/generator.py
@@ -4,6 +4,6 @@ from dmoj.utils.aux_files import compile_with_auxiliary_files
 
 
 class GeneratorManager:
-    def get_generator(self, filenames, flags, lang=None, compiler_time_limit=None):
+    def get_generator(self, filenames, flags, lang=None, compiler_time_limit=None, cached=True):
         filenames = list(map(os.path.abspath, filenames))
-        return compile_with_auxiliary_files(filenames, lang, compiler_time_limit)
+        return compile_with_auxiliary_files(filenames, flags, lang, compiler_time_limit, cached)

dmoj/problem.py
@@ -243,6 +243,7 @@ class TestCase:
         time_limit = env.generator_time_limit
         memory_limit = env.generator_memory_limit
         compiler_time_limit = env.generator_compiler_time_limit
+        cached = True
         lang = None  # Default to C/C++
 
         base = get_problem_root(self.problem.id)
@@ -266,6 +267,7 @@ class TestCase:
             time_limit = gen.time_limit or time_limit
             memory_limit = gen.memory_limit or memory_limit
             compiler_time_limit = gen.compiler_time_limit or compiler_time_limit
+            cached = gen.get('cached', True)
             lang = gen.language
 
         if not isinstance(filenames, list):
@@ -273,7 +275,7 @@ class TestCase:
 
         filenames = [os.path.join(base, name) for name in filenames]
         executor = self.problem.generator_manager.get_generator(filenames, flags, lang=lang,
-                                                                compiler_time_limit=compiler_time_limit)
+                                                                compiler_time_limit=compiler_time_limit, cached=cached)
 
         # convert all args to str before launching; allows for smoother int passing
         args = map(str, args)

dmoj/utils/aux_files.py
@@ -11,7 +11,7 @@ def mktemp(data):
     return tmp
 
 
-def compile_with_auxiliary_files(filenames, lang=None, compiler_time_limit=None):
+def compile_with_auxiliary_files(filenames, flags, lang=None, compiler_time_limit=None, cached=True):
     from dmoj.executors import executors
     from dmoj.executors.compiled_executor import CompiledExecutor
 
@@ -45,10 +45,15 @@ def compile_with_auxiliary_files(filenames, lang=None, compiler_time_limit=None)
     if issubclass(executor, CompiledExecutor):
         executor = type('Executor', (executor,), {'compiler_time_limit': compiler_time_limit})
 
+    if hasattr(executor, 'flags'):
+        # We shouldn't be mutating the base class flags.
+        # See <https://github.com/DMOJ/judge-server/issues/174>.
+        executor = type('FlaggedExecutor', (executor,), {'flags': flags + list(executor.flags)})
+
     # Optimize the common case.
     if use_cpp or use_c:
-        # Some auxilary files (like those using testlib.h) take an extremely long time to compile, so we cache them.
-        executor = executor('_aux_file', None, aux_sources=sources, cached=True)
+        # Some auxiliary files (like those using testlib.h) take an extremely long time to compile, so we cache them.
+        executor = executor('_aux_file', None, aux_sources=sources, cached=cached)
     else:
         if len(sources) > 1:
             raise InternalError('non-C/C++ auxilary programs cannot be multi-file')